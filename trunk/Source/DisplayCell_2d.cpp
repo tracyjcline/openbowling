0b0ee51372b0df7dedc0bd5b5b85d52cf2e31dd8
michcaptk <michcaptk@1a4918ac-d60d-11de-829d-073b792d0d12>
2010-05-10 17:46:03 +0000

    Modifications as initial development continues.

--- trunk/Source/DisplayCell_2d.cpp
+++ trunk/Source/DisplayCell_2d.cpp
@@ -1,3 +1,5 @@
+#include <QPainter>
+
 #include "DisplayCell_2d.h"
 
 DisplayCell_2d::DisplayCell_2d( qreal x, qreal y, qreal width, qreal height, QGraphicsItem *cellParent )
@@ -7,4 +9,19 @@ DisplayCell_2d::DisplayCell_2d( qreal x, qreal y, qreal width, qreal height, QGr
 
 DisplayCell_2d::~DisplayCell_2d()
 {
+}
+
+void DisplayCell_2d::paint( QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget )
+{
+	Q_UNUSED( option );
+	Q_UNUSED( widget );
+
+	const QColor *brushColor = new QColor( 0, 0, 0 );
+	const QBrush *brush = new QBrush( *brushColor );
+
+	painter->setPen( QPen( *brush, 3.0, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin ) );
+	painter->drawRect( this->boundingRect() );
+
+	delete brushColor;
+	delete brush;
 }
