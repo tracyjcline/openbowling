15011be459b9e14af1475e5c6429ab7b52711a9d
michcaptk <michcaptk@1a4918ac-d60d-11de-829d-073b792d0d12>
2010-05-12 21:04:17 +0000

    Basic frame screen with TCP enabled new scores is now complete.

--- trunk/Source/DisplayCell_2d.cpp
+++ trunk/Source/DisplayCell_2d.cpp
@@ -2,6 +2,9 @@
 
 #include "DisplayCell_2d.h"
 
+#define FONT_MAX_START_SIZE_F 72.0
+#define FONT_DECREMENT_PERCENT 0.80
+
 DisplayCell_2d::DisplayCell_2d( qreal x, qreal y, qreal width, qreal height, QGraphicsItem *cellParent )
 	: QGraphicsRectItem( x, y, width, height, cellParent )
 {
@@ -26,6 +29,25 @@ void DisplayCell_2d::paint( QPainter *painter, const QStyleOptionGraphicsItem *o
 
 	delete penBrush;
 
+	QFont newFont( painter->font() );
+	bool fontTooBig = true;
+	qreal fSize = FONT_MAX_START_SIZE_F;
+	while( fontTooBig )
+	{
+		newFont.setPointSizeF( fSize );
+		QFontMetrics fMetrics( newFont );
+		QRect fBound = fMetrics.boundingRect( displayValue );
+		if( fBound.width() > this->boundingRect().width()
+			|| fBound.height() > this->boundingRect().height() )
+		{
+			fSize *= FONT_DECREMENT_PERCENT;
+		}
+		else
+		{
+			fontTooBig = false;
+		}
+	}
+	painter->setFont( newFont );
 	painter->drawText( this->boundingRect(), displayValue, QTextOption(Qt::AlignCenter) );
 }
 
